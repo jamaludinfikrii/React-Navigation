e4952b924e03ee04bdf80abb93984b7f22a5f88e
Fikri <jamesfikrii@gmail.com>
2019-05-10 11:35:35 +0700

    react-native-map

--- src/screens/PemesananScree.js
+++ src/screens/PemesananScree.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Button
 } from "react-native";
 import MapView,{Marker} from 'react-native-maps'
-M
 const styles = StyleSheet.create({
     container: {
         ...StyleSheet.absoluteFillObject,
@@ -16,30 +16,45 @@ const styles = StyleSheet.create({
     },
 });
 
-onBtnClick = () => {
-    navigator.geolocation.getCurrentPosition(value => {
-        console.log(value)
-    }, err => {
-        console.log(err)
-    })
-}
+
+
 
 
 class Pemesanan extends Component {
+    state = {location : null}
+    onBtnClick = () => {
+        navigator.geolocation.getCurrentPosition(value => {
+            console.log(value)
+            this.setState({location : {
+                latitude : value.coords.latitude,
+                longitude : value.coords.longitude,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121
+            }})
+        }, err => {
+            console.log(err)
+        })
+    }
+    
     render() {
+        const initial = {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+        }
+        const obj = this.state.location ? this.state.location : initial
         return (
             <View style={styles.container}>
+                <View style={{marginTop : 30 , zIndex : 1}}>
+                    <Button title='Get Current Location' onPress={this.onBtnClick}/>
+                </View>
                 <MapView
                 style={styles.map}
-                region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
-                }}
+                region={obj}
                 >
 
-                    
+                <Marker coordinate={obj}/>
                 </MapView>
             </View>
         );
